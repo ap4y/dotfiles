2453f0e04ff870405829a82092cfaea1e9a997ea
ap4y <lod@pisem.net>
2013-07-14 12:12:08 +1200

    Install script added

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+ln -sf ~/.dotfiles/gemrc ~/.gemrc
+ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/gitignore ~/.gitignore_global
+ln -sf ~/.dotfiles/gitk ~/.gitk
+ln -sf ~/.dotfiles/jshintrc ~/.jshintrc
+ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/zshrc ~/.zshrc
+
+ln -sf ~/.dotfiles/vim ~/.vim
+ln -sf ~/.dotfiles/vim/gvimrc ~/.gvimrc
+ln -sf ~/.dotfiles/vim/vimrc ~/.vimrc
