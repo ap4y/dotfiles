4ee247c4a59cd5b10bf08ec306f33648f2d64dec
ap4y <lod@pisem.net>
2014-01-30 16:28:13 +1300

    Updated custom emacs settings

--- personal/custom.el
+++ personal/custom.el
@@ -6,35 +6,68 @@
 
 ;; visual settings
 (menu-bar-mode -1)
-(add-to-list 'default-frame-alist '(font . "Inconsolata-dz-24"))
+(scroll-bar-mode -1)
+(add-to-list 'default-frame-alist '(font . "Inconsolata dz-14"))
 
 ;; mac os meta rebind
-(setq mac-command-modifier 'meta)
-(setq mac-option-modifier 'super)
+;; (setq mac-command-modifier 'meta)
+;; (setq mac-option-modifier 'super)
 
 ;; additional packages
 (prelude-ensure-module-deps '(dash-at-point multi-term
-                                            handlebars-mode auto-complete yasnippet
-                                            elixir-mode ag rspec-mode))
+                                            handlebars-mode auto-complete
+                                            yasnippet elixir-mode ag
+                                            rspec-mode js2-mode))
 
 ;; multi-term settings
 (setq multi-term-program "/bin/zsh")
-
-(global-set-key (kbd "C-c t") 'multi-term-next)
 (global-set-key (kbd "C-c T") 'multi-term)
 
-;; 2 spaces indentation for js
-(setq js-indent-level 2)
+;; js2-mode settings
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(setq js2-basic-offset 2)
 
 ;; don't use rake in rspec-mode
 (setq rspec-use-rake-when-possible nil)
 (setq rspec-use-zeus-when-possible t)
 
-;; enforcing utf-8
-(prefer-coding-system       'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
+;; enforcing utf-8 in terminal
+(add-hook 'term-exec-hook
+          (function
+           (lambda ()
+             (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))))
+
+;; custom align-regexp rules
+(defun  align-commas (beg end)
+  (interactive "r")
+  (align-regexp beg end ",\\(\\s-*\\)" 1 1 t))
+
+(defun align-colons (beg end)
+  (interactive "r")
+  (align-regexp beg end ":\\(\\s-*\\)" 1 1 t))
+
+(defun align-hash (beg end)
+  (interactive "r")
+  (align-regexp beg end "\\(\\s-*\\)\=\>\\(\\s-*\\)" 1 1 t))
+
+;; irony-mode
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/personal/irony-mode/elisp"))
+
+(require 'auto-complete-config)
+(ac-config-default)
+
+(require 'auto-complete)
+(require 'yasnippet)
+(require 'irony)
+
+(irony-enable '(ac))
+
+(defun custom-objc-hook ()
+  (yas/minor-mode-on)
+  (auto-complete-mode 1)
+  (irony-mode 1))
+
+(add-hook 'objc-mode-hook 'custom-objc-hook)
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not cl-functions)
