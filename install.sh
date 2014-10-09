6f99725fe10a76a9ab7f3da96dcba2d9b8719cfe
ap4y <lod@pisem.net>
2014-10-09 16:28:12 +1300

    Update install script

--- install.sh
+++ install.sh
@@ -2,23 +2,15 @@
 
 SOURCE_PATH="$HOME/.dotfiles"
 
-ln -sf $SOURCE_PATH/gemrc $HOME/.gemrc
-ln -sf $SOURCE_PATH/gitconfig $HOME/.gitconfig
-ln -sf $SOURCE_PATH/gitignore $HOME/.gitignore_global
-ln -sf $SOURCE_PATH/gitk $HOME/.gitk
-ln -sf $SOURCE_PATH/jshintrc $HOME/.jshintrc
-ln -sf $SOURCE_PATH/tmux.conf $HOME/.tmux.conf
-ln -sf $SOURCE_PATH/tmux-osx.conf $HOME/.tmux-osx.conf
-ln -sf $SOURCE_PATH/zshrc $HOME/.zshrc
-ln -sf $SOURCE_PATH/zpreztorc $HOME/.zpreztorc
-ln -sf $SOURCE_PATH/ctags $HOME/.ctags
-ln -sf $SOURCE_PATH/bash_profile $HOME/.bash_profile
+for f in $(ls $SOURCE_PATH -I install.sh -I scripts)
+do
+    ln -sf $SOURCE_PATH/$f $HOME/.$f
+done
 
-ln -sf $SOURCE_PATH/vim $HOME/.vim
 ln -sf $SOURCE_PATH/vim/gvimrc $HOME/.gvimrc
 ln -sf $SOURCE_PATH/vim/vimrc $HOME/.vimrc
 
 # emacs with prelude
 # curl -L http://git.io/epre | sh
-ln -sf $SOURCE_PATH/custom.el $HOME/.emacs.d/personal/custom.el
-cp prelude-modules.el $HOME/.emacs.d
+ln -sf $SOURCE_PATH/emacs/custom.el $HOME/.emacs.d/personal/custom.el
+cp emacs/prelude-modules.el $HOME/.emacs.d
