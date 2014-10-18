ab79958528d2f7bc51bae56f499060e00b3626ce
ap4y <lod@pisem.net>
2014-10-18 20:31:29 +1300

    Update install script

--- install.sh
+++ install.sh
@@ -2,15 +2,10 @@
 
 SOURCE_PATH="$HOME/.dotfiles"
 
-for f in $(ls $SOURCE_PATH -I install.sh -I scripts)
+for f in $(ls $SOURCE_PATH -I install.sh -I scripts -I vim)
 do
     ln -sf $SOURCE_PATH/$f $HOME/.$f
 done
 
 ln -sf $SOURCE_PATH/vim/gvimrc $HOME/.gvimrc
 ln -sf $SOURCE_PATH/vim/vimrc $HOME/.vimrc
-
-# emacs with prelude
-# curl -L http://git.io/epre | sh
-ln -sf $SOURCE_PATH/emacs/custom.el $HOME/.emacs.d/personal/custom.el
-cp emacs/prelude-modules.el $HOME/.emacs.d
