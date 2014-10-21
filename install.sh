e55028db15a11171b6951a36e8ae8bce4f04d583
ap4y <lod@pisem.net>
2014-10-21 19:55:36 +1300

    Make install script compatible with osx

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 
 SOURCE_PATH="$HOME/.dotfiles"
 
-for f in $(ls $SOURCE_PATH -I install.sh -I scripts -I vim)
+for f in $(find . -type file -maxdepth 1 ! -name 'install.sh' ! -name '[.]*' -exec basename {} \;)
 do
     ln -sf $SOURCE_PATH/$f $HOME/.$f
 done
+
+ln -sf $SOURCE_PATH/vim/ $HOME/.vim
+ln -sf $SOURCE_PATH/emacs.d/ $HOME/.emacs.d
 
 ln -sf $SOURCE_PATH/vim/gvimrc $HOME/.gvimrc
 ln -sf $SOURCE_PATH/vim/vimrc $HOME/.vimrc
