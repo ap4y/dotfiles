33dfd70e3aa1897bb8af10fa91456caf1b220648
ap4y <lod@pisem.net>
2015-02-05 14:35:52 +1300

    Update intall script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 SOURCE_PATH="$HOME/.dotfiles"
 
-for f in $(find . -type file -maxdepth 1 ! -name 'install.sh' ! -name '[.]*' -exec basename {} \;)
+for f in $(find . -maxdepth 1 -type f ! -name 'install.sh' ! -name '[.]*' -exec basename {} \;)
 do
     ln -sf $SOURCE_PATH/$f $HOME/.$f
 done
@@ -12,3 +12,6 @@ ln -sf $SOURCE_PATH/emacs.d/ $HOME/.emacs.d
 
 ln -sf $SOURCE_PATH/vim/gvimrc $HOME/.gvimrc
 ln -sf $SOURCE_PATH/vim/vimrc $HOME/.vimrc
+
+ln -sf $SOURCE_PATH/bin/ $HOME/bin
+ln -sf $SOURCE_PATH/colors/ $HOME/.colors
