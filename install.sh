2ca3a523602c0c5cc9e128b8dafc3e16689a69e2
ap4y <lod@pisem.net>
2015-02-20 15:11:42 +1300

    Reimplement install script using GNU Stow

--- install.sh
+++ install.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
-SOURCE_PATH="$HOME/.dotfiles"
-
-for f in $(find . -maxdepth 1 -type f ! -name 'install.sh' ! -name '[.]*' -exec basename {} \;)
+packages=( bash tmux xorg config emacs vim weechat others )
+for package in "${packages[@]}"
 do
-    ln -sf $SOURCE_PATH/$f $HOME/.$f
+    stow $package
 done
-
-ln -sf $SOURCE_PATH/vim/ $HOME/.vim
-ln -sf $SOURCE_PATH/emacs.d/ $HOME/.emacs.d
-
-ln -sf $SOURCE_PATH/vim/gvimrc $HOME/.gvimrc
-ln -sf $SOURCE_PATH/vim/vimrc $HOME/.vimrc
-
-ln -sf $SOURCE_PATH/bin/ $HOME/bin
-ln -sf $SOURCE_PATH/colors/ $HOME/.colors
