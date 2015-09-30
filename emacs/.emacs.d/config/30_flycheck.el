4a9256706c124f8f6af571d37bf37020ca32d1b7
ap4y <lod@pisem.net>
2015-09-30 14:13:26 +1300

    Use custom fringe glyphs with flycheck

--- emacs/.emacs.d/config/30_flycheck.el
+++ emacs/.emacs.d/config/30_flycheck.el
@@ -1,2 +1,12 @@
 (require 'flycheck)
+(require 'fringe-helper)
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+(fringe-helper-define 'flycheck-fringe-bitmap-double-arrow nil
+  "........"
+  "........"
+  "........"
+  ".X.X...."
+  "..X.X..."
+  ".X.X...."
+  "........")
