8905b3313ce81d809c5157100b8ffc079a4c9228
ap4y <lod@pisem.net>
2016-05-24 15:37:08 +1200

    Add objc mode config for the emacs

---
+++ emacs/.emacs.d/config/50_objc.el
@@ -0,0 +1,24 @@
+(require 'clang-format)
+
+(defun objc-mode-defaults ()
+  (add-to-list 'projectile-other-file-alist '("h" "m"))
+  (add-to-list 'projectile-other-file-alist '("m" "h"))
+
+  ;; clang-format on save
+  (add-hook 'before-save-hook 'clang-format-buffer nil t)
+
+  (c-set-style "llvm.org")
+  (setq c-basic-offset 4)
+  (setq whitespace-line-column 120)
+
+  ;; CamelCase aware editing operations
+  (subword-mode +1))
+
+(add-hook 'objc-mode-hook 'objc-mode-defaults)
+
+(add-to-list 'magic-mode-alist
+             `(,(lambda ()
+                  (and (string= (file-name-extension buffer-file-name) "h")
+                       (re-search-forward "@\\<interface\\>"
+                                          magic-mode-regexp-match-limit t)))
+               . objc-mode))
