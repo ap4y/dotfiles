91ed04ef9e0c40eacadcbcc0d341ec8f4289282a
ap4y <lod@pisem.net>
2016-05-24 15:44:34 +1200

    Update cpp mode config for emacs

--- emacs/.emacs.d/config/50_cpp.el
+++ emacs/.emacs.d/config/50_cpp.el
@@ -1,12 +1,25 @@
+(require 'clang-format)
+
+(c-add-style "llvm.org"
+             '((fill-column . 80)
+               (c++-indent-level . 2)
+               (c-basic-offset . 2)
+               (indent-tabs-mode . nil)
+               (c-offsets-alist . ((arglist-intro . ++)
+                                   (innamespace . 0)
+                                   (member-init-intro . ++)))))
+
 (defun c++-mode-defaults ()
+  (add-to-list 'projectile-other-file-alist '("h" "cc"))
+  (add-to-list 'projectile-other-file-alist '("cc" "h"))
+
   (custom-set-variables
-   '(flycheck-c/c++-googlelint-executable "/usr/bin/cpplint")
-   '(flycheck-googlelint-filter "-legal/copyright")
-   '(flycheck-googlelint-root "lib")
    '(flycheck-gcc-language-standard "c++11")
    '(flycheck-clang-language-standard "c++11"))
-  (require 'flycheck-google-cpplint)
-  (flycheck-add-next-checker 'c/c++-gcc 'c/c++-googlelint))
+
+  ;; clang-format on save
+  (add-hook 'before-save-hook 'clang-format-buffer nil t)
+
+  (c-set-style "llvm.org"))
 
 (add-hook 'c++-mode-hook 'c++-mode-defaults)
-(add-hook 'c-mode-common-hook 'google-set-c-style)
