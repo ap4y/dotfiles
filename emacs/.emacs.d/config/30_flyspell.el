2008bfeb5aee3b4d582342189d615b21b765c8ee
ap4y <mail@ap4y.me>
2017-11-21 23:07:24 +1300

    Fix typo in flyspell config

--- emacs/.emacs.d/config/30_flyspell.el
+++ emacs/.emacs.d/config/30_flyspell.el
@@ -1,5 +1,5 @@
 (use-package flyspell
-  :commands flycheck-mode
+  :commands enable-flyspell-mode
   :config
   (setq ispell-program-name "aspell" ; use aspell instead of ispell
         ispell-extra-args '("--sug-mode=ultra")))
