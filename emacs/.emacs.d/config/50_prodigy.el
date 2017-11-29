41e2eed45a3bfb998f437eff82ed50e9838a8ca1
ap4y <mail@ap4y.me>
2017-11-29 14:16:16 +1300

    Use host net in docker in prodigy

--- emacs/.emacs.d/config/50_prodigy.el
+++ emacs/.emacs.d/config/50_prodigy.el
@@ -5,24 +5,24 @@
   (prodigy-define-service
     :name "MySQL container"
     :command "docker"
-    :args '("run" "--rm" "-p" "3306:3306" "-e" "MYSQL_ROOT_PASSWORD=root" "mysql"))
+    :args '("run" "--rm" "--net" "host" "-e" "MYSQL_ROOT_PASSWORD=root" "mysql"))
 
   (prodigy-define-service
     :name "Postgres container"
     :command "docker"
-    :args '("run" "--rm" "-p" "5432:5432" "postgres"))
+    :args '("run" "--rm" "--net" "host" "postgres"))
 
   (prodigy-define-service
     :name "Redis container"
     :command "docker"
-    :args '("run" "--rm" "-p" "6379:6379" "redis"))
+    :args '("run" "--rm" "--net" "host" "redis"))
 
   (prodigy-define-service
     :name "ETCD container"
     :command "docker"
-    :args '("run" "--rm" "-p" "4001:4001" "microbox/etcd" "-name" "localhost"))
+    :args '("run" "--rm" "--net" "host" "microbox/etcd" "-name" "localhost"))
 
   (prodigy-define-service
     :name "ES container"
     :command "docker"
-    :args '("run" "--rm" "-p" "9200:9200" "elasticsearch:1.6")))
+    :args '("run" "--rm" "--net" "host" "elasticsearch:1.6")))
