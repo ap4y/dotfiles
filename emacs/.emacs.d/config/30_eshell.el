a15be577b5624c1d3fd46ebf57ff8f5de5584ba0
ap4y <mail@ap4y.me>
2017-11-29 14:15:49 +1300

    Support doas in tramp

--- emacs/.emacs.d/config/30_eshell.el
+++ emacs/.emacs.d/config/30_eshell.el
@@ -1,6 +1,15 @@
 (use-package eshell
   :commands (eshell eshell-command)
   :bind (("C-x m" . eshell))
+  :preface
+  (defun ap4y/truncate-eshell-buffers ()
+    "Truncates all eshell buffers"
+    (interactive)
+    (save-current-buffer
+      (dolist (buffer (buffer-list t))
+        (set-buffer buffer)
+        (when (eq major-mode 'eshell-mode)
+          (eshell-truncate-buffer)))))
   :config
   (setq eshell-prompt-function (lambda nil
                                  (concat
@@ -13,14 +22,15 @@
   (setq eshell-history-size (* 10 1024))
   (setq eshell-hist-ignoredups t)
 
-  (defun ap4y/truncate-eshell-buffers ()
-    "Truncates all eshell buffers"
-    (interactive)
-    (save-current-buffer
-      (dolist (buffer (buffer-list t))
-        (set-buffer buffer)
-        (when (eq major-mode 'eshell-mode)
-          (eshell-truncate-buffer)))))
-
   (setq ap4y/eshell-truncate-timer
         (run-with-idle-timer 5 t #'ap4y/truncate-eshell-buffers)))
+
+(use-package tramp
+  :config
+  (add-to-list 'tramp-methods
+               '("doas"
+                 (tramp-login-program        "doas")
+                 (tramp-login-args           (("-u" "%u") ("-s")))
+                 (tramp-remote-shell         "/bin/sh")
+                 (tramp-remote-shell-args    ("-c"))
+                 (tramp-connection-timeout 10))))
