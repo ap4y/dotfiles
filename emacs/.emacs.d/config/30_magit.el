b6012772ffa32a2b345f894e9e39bbe0933acf5d
ap4y <mail@ap4y.me>
2017-12-01 12:00:23 +1300

    Improve magit config

--- emacs/.emacs.d/config/30_magit.el
+++ emacs/.emacs.d/config/30_magit.el
@@ -8,9 +8,8 @@
          ("C-x G" . magit-status-with-prefix))
   :config
   (setq magit-push-always-verify nil)
-  (setq magit-completing-read-function 'ivy-completing-read)
-
   (setq git-commit-summary-max-length 80)
+  (setq vc-handled-backends (delq 'Git vc-handled-backends))
 
   ;; (setq magit-display-buffer-function
   ;;       (lambda (buffer)
