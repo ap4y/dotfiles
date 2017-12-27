d8a2a8b464c1c8686b31e9866309885f1e2a97ac
ap4y <mail@ap4y.me>
2017-12-28 10:07:27 +1300

    Update several chip configs

--- chip/config.sh
+++ chip/config.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-
 cat bashrc >> ~/.bashrc
 cp -R .mutt ~
 cp -R .tmux.conf ~
 cp -R .Xresources ~
+cp -R .xinitrc ~
 
 mkdir ~/.irssi
 cp -R scripts ~/.irssi
--- chip/install.sh
+++ chip/install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
-apt-get update && apt-get upgrade
-apt-get install openssh-server openssh-client git kbd locales console-setup tmux ranger mutt irssi
+apt-get update -y && apt-get upgrade -y
+apt-get install -y openssh-server openssh-client git kbd locales console-setup tmux \
+        ranger mutt irssi firmware-realtek crda apt-transport-https xterm
+apt-get remove -y network-manager lightdm awesome pocket-wm xfce4-session
+apt-get autoremove
 
 cp tewi-medium-11.psf /usr/share/consolefonts/
 
@@ -10,7 +13,7 @@ dpkg-reconfigure locales
 dpkg-reconfigure console-setup
 
 systemctl set-default multi-user.target
-systemctl enable ssh.socket
+systemctl enable ssh.
 
 git clone https://github.com/editkid/chip-battery-status.git
 cd chip-battery-status
@@ -18,3 +21,17 @@ cd chip-battery-status
 cd .. && rm -rf chip-battery-status
 
 cp tmux-notify /usr/local/bin/
+
+echo 'FONT=tewi-medium-11.psf' >> /etc/default/console-setup
+
+loadkeys pocketchip.kmap
+keydump > keydump.kmap
+mv keydump.kmap /usr/share/keymaps/pocketchip.kmap
+
+echo "/dev/mtdblock3 0 0x400000 0x4000" | sudo tee -a /etc/fw_env.config
+fw_setenv bootargs 'root=ubi0:rootfs rootfstype=ubifs rw ubi.mtd=4 quiet lpj=501248 loglevel=3'
+
+systemctl disable pocketchip-warn15.timer
+systemctl disable pocketchip-warn05.timer
+systemctl disable pocketchip-off00.timer
+systemctl disable pocketchip-batt.timer
