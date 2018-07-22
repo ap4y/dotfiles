71de4b29b30578af12b63ebc49d34d269efce4bc
ap4y <mail@ap4y.me>
2018-07-23 11:17:17 +1200

    Add various keybindings

--- emacs/.emacs.d/config/01_keybidings.el
+++ emacs/.emacs.d/config/01_keybidings.el
@@ -3,6 +3,7 @@
 (global-set-key (kbd "C-c r") 'rename-buffer-and-file) ;; Rename buffer
 (global-set-key (kbd "C-c j") 'top-join-line)          ;; Join lines
 (global-set-key (kbd "C-c k") 'custom-kill-whole-line) ;; Kill line
+(global-set-key (kbd "C-z") 'custom-kill-whole-line)   ;; Kill line
 
 ;; Adaptive beginning of the line
 (global-set-key [remap move-beginning-of-line] 'custom-move-beginning-of-line)
@@ -12,3 +13,25 @@
                                 (interactive)
                                 (yank)
                                 (indent-region (region-beginning) (region-end))))
+
+(use-package avy
+  :ensure t
+  :bind (("M-g l" . avy-goto-line)
+         ("M-g c" . avy-goto-char-2)))
+
+(use-package dumb-jump
+  :ensure t
+  :bind (("M-g j" . dumb-jump-go)
+         ("M-g o" . dumb-jump-go-other-window)))
+
+(use-package ace-window
+  :ensure t
+  :bind (("s-r" . ace-window))
+  :config
+  (setq aw-dispatch-always t)
+  (setq aw-scope 'frame))
+
+(use-package expand-region
+  :ensure t
+  :bind (("C-=" . er/expand-region)
+         ("C--" . er/contract-region)))
