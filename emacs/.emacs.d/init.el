147d45a2a05a2e6952f0ae43a18cbd4834bdf2d0
ap4y <mail@ap4y.me>
2019-04-29 15:09:12 +1200

    Update exmw-randr config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -583,7 +583,7 @@
 
   ;; multi-monitor
   (require 'exwm-randr)
-  (setq exwm-randr-workspace-output-plist '(3 "HDMI-0" 0 "DP-0" 1 "DP-0" 2 "DP-0"))
+  (setq exwm-randr-workspace-output-plist '(3 "HDMI-A-0" 0 "DisplayPort-1" 1 "DisplayPort-1" 2 "DisplayPort-1"))
   (add-hook 'exwm-randr-screen-change-hook
             (lambda ()
               ;; (start-process-shell-command "xrandr" nil "xrandr --output DP-0 --auto --primary")
